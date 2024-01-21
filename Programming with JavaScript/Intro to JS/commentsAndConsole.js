b2837fbb9e349d3bd35db327ea1c908f05aa2ce8
Manan Arora <mananarora313@gmail.com>
2024-01-21 15:09:30 -0600

    started with JS

---
+++ Programming with JavaScript/Intro to JS/commentsAndConsole.js
@@ -0,0 +1,17 @@
+// this is a single line comment in JS
+console.log('Hello World')
+
+/**
+ * 
+ * This is a multiple line
+ * comment in JS
+ */
+
+console.log("%cHello World", "color: blue; font-size:40px");
+
+// Output Multiple words into the console using +
+console.log("Hello" + "there," + " Manan")
+
+// Output multiple words into the console using ,
+// With comma (,) even if we don't add space then also space is being added
+console.log("Manan", "Arora");
