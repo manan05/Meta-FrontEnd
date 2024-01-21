1ce0050c37af2c5c1f355d8f3ad9dc6f2e62a7e3
Manan Arora <mananarora313@gmail.com>
2024-01-21 17:43:31 -0600

    updated comments and Console

--- Programming with JavaScript/Intro to JS/commentsAndConsole.js
+++ Programming with JavaScript/Intro to JS/commentsAndConsole.js
@@ -1,3 +1,5 @@
+// Syntax
+
 // this is a single line comment in JS
 console.log('Hello World')
 
@@ -15,3 +17,37 @@ console.log("Hello" + "there," + " Manan")
 // Output multiple words into the console using ,
 // With comma (,) even if we don't add space then also space is being added
 console.log("Manan", "Arora");
+
+
+// Variables 
+
+// save the variable and us eit in future
+var person;
+person = "Manan"
+console.log("Hello" ,person)
+
+var greeting = "Hello"
+console.log(greeting , person);
+
+person = "Sahil"
+console.log(person, greeting);
+
+// Exercise: Declaring Varibales, 
+
+var petDog = 'Rex'
+var petCat = 'Pepper'
+console.log(petDog);
+console.log(petCat);
+console.log('My pet dog\'s name is:', petDog);
+console.log('My pet cat\'s name is:', petCat);
+
+var catSound = 'purr'
+var dogSound = 'woof'
+console.log(petDog , 'says' , dogSound);
+console.log(petCat , 'says' , catSound);
+
+catSound = 'Meow'
+console.log(petCat , 'now says' , catSound);
+
+var petDog = 'Rex'; // Task 1 solution
+console.log(petDog);
