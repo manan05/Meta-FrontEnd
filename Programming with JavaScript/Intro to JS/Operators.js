d1ebd10065d2ca98fb68dee1d4114e91ec650ce8
Manan Arora <mananarora313@gmail.com>
2024-01-21 20:51:57 -0600

    read about operators in JS

---
+++ Programming with JavaScript/Intro to JS/Operators.js
@@ -0,0 +1,49 @@
+/*Arithmetic operators in JS:
+    + addiiton
+    - subtraction
+    * multiplication
+    / division
+*/
+
+/*Comparison operators in JS:
+    > greater than
+    < less than
+    == equal 
+*/
+
+console.log(2 + 2);
+console.log(1 + 2 + 3 + 4 + 5);
+console.log(20 - 18);
+console.log(2 * 3);
+console.log(8 / 1);
+console.log(3 > 2);
+console.log(2 > 3);
+console.log(10 == 10);
+
+
+/* Additional operators in JS:
+    Logical AND operator: &&
+    Logical OR operator: ||
+    Modulus operator: %
+    Logical NOT operator: !
+    equality operator: ==
+    strict equality operator: ===
+    inequality operator: !=
+    strict inequality operator: !==
+    addition assignment operator: +=
+    the concatenation and assignment operator: +=
+*/
+
+var currentTime = 10
+console.log(currentTime > 9 && currentTime < 17)
+
+console.log(5 != "5")
+console.log(5 == "5")
+
+
+// combining 2 strings using the + operator
+
+console.log('inter' + 'net');
+
+// combinging strings and numbers using the + operator
+ console.log(365 + ' days')
