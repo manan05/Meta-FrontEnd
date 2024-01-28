98b53d77c64d60b2d8a499deba7fc7a2d9220e0f
Manan Arora <mananarora313@gmail.com>
2024-01-28 15:42:41 -0600

    updated errorHandling.js with Exercise

--- Programming with JavaScript/Intro to JS/errorHandling.js
+++ Programming with JavaScript/Intro to JS/errorHandling.js
@@ -35,8 +35,52 @@ var man;
 console.log(man); // undefined
 
 // also and object attribute that does not exist
- var game = {
-    speed : 500,
-    score : 1
- }
- console.log(game.Score); // undefined
+var game = {
+    speed: 500,
+    score: 1
+}
+console.log(game.Score); // undefined
+
+
+// Exercise: Error prevention
+function addTwoNums(a, b) {
+    try {
+        if (typeof a != 'number') {
+            throw new ReferenceError('the first argument is not a number');
+        } else if (typeof b != 'number') {
+            throw new ReferenceError('the second argument is not a number');
+        } else {
+            console.log(a + b);
+        }
+    } catch (err) {
+        console.log("Error!", err);
+    }
+
+}
+
+addTwoNums(5, '5');
+console.log("It still works");
+
+
+// Exercise: Defensive programming
+
+function letterFinder(word, match) {
+    var condition1 = typeof word == 'string' && word.length >= 2;
+    var condition2 = typeof match == 'string' && match.length == 1;
+    if (condition1 && condition2) {
+        for (var i = 0; i < word.length; i++) {
+            if (word[i] == match) {
+                //if the current character at position i in the word is equal to the match
+                console.log('Found the', match, 'at', i)
+            } else {
+                console.log('---No match found at', i)
+            }
+        }
+    }
+    else{
+        console.log('Please pass correct arguments to the function.');
+    }
+}
+
+letterFinder(1,2);
+letterFinder('hello', 'o');
