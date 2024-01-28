ea530070daa134986657896a6c25b2d6ae4a80a7
Manan Arora <mananarora313@gmail.com>
2024-01-28 01:32:26 -0600

    completed exercise

--- Programming with JavaScript/Intro to JS/strings.js
+++ Programming with JavaScript/Intro to JS/strings.js
@@ -54,11 +54,11 @@ clothes.push("pant");
 clothes.push("tShirt");
 clothes.push("tracksuit");
 clothes.push("shoes");
-
+console.log(clothes);
 clothes.pop()
-
+console.log(clothes);
 clothes.push("Sneakers");
-
+console.log(clothes);
 console.log(clothes[2]);
 
 var favCar = {};
@@ -66,3 +66,66 @@ var favCar = {};
 favCar.color = "black";
 favCar.convertible = true;
 console.log(favCar);
+
+// Object Methods
+
+var car = {};
+car.color = "red";
+
+// adding a method to the car object so that it can be called as car.turnkey()
+car.turnkey = function() {
+  console.log('engine running');
+}
+
+var car = {};
+car.mileage = 98765;
+car.color = 'red';
+console.log(car);
+// adding the turnkey method to the car object
+car.turnkey = function() {
+  console.log('the engine is running');
+}
+console.log(car);
+
+car.lightsOn = function(){
+  console.log('The lights are on');
+}
+
+console.log(car);
+car.turnkey();
+car.lightsOn();
+
+// typeOf operator
+
+var test = typeof("what is this?");
+console.log(test);
+
+var test = typeof(10);
+console.log(test);
+
+var test = typeof(3.14);
+console.log(test);
+
+var test = typeof(true);
+console.log(test);
+
+var test = typeof(false);
+console.log(test);
+
+var test = typeof('1 < 2');
+console.log(test);
+
+var test = typeof([1,2,3,4]);
+console.log(test);
+
+var test = typeof({firstProperty:1});
+console.log(test);
+
+var test = typeof(function man(){console.log('manan');});
+console.log(test);
+
+var test = typeof(null);
+console.log(test);
+
+var test = typeof(undefined);
+console.log(test);
