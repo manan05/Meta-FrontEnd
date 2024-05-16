df8eb45e4b26abb2a868beb53c5c9630c64300ba
Manan Arora <mananarora313@gmail.com>
2024-05-16 16:00:54 -0500

    updated with forOf Loops examples

--- Programming with JavaScript/Intro to JS/Week 3/forOfLoops.js
+++ Programming with JavaScript/Intro to JS/Week 3/forOfLoops.js
@@ -12,10 +12,45 @@ const car = {
 // but on the contrary, arrays are iterable,
 const letters = ['a', 'b', 'c', 'd'];
 for (var letter of letters){
-    console.log(letter);
+    // console.log(letter);
 }
 
 // We can use this concept to iterate over an object...
 
 // some methods, Object.keys(), Object.values(), Object.entries()
+
+// Object.keys() method
+
+const car2 = {
+    speed: 200,
+    color: "red"
+}
+
+console.log(Object.keys(car2)); // [ 'speed', 'color' ]
+
+
+// Object.values() method
+
+console.log(Object.values(car2)); // [ 200, 'red' ]
+
+
+// Object.entries() method
+
+console.log(Object.entries(car2)); // [ ['speed', 200 ], [ 'color','red' ] ]
+
+
+// Example:
+
+// ingredients that  we need to loop over any object's own property keys and values
+var clothingItem = {
+    price: 50,
+    color: 'beige',
+    material: 'cotton',
+    season: 'autumn'
+}
+
+for(const key of Object.keys(clothingItem)){
+    console.log(key, ":" , clothingItem[key]);
+}
+
 
